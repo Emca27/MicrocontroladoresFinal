9aa0f4e07fc2214c1aecb25f60e1d5965e4785d4
m-ort <m-ort@LAPTOP-QP0H3BHR>
2020-05-18 21:14:30 -0500

    Se agregaron las bases del LCD

--- Final Micros.asm
+++ Final Micros.asm
@@ -18,12 +18,18 @@ configura movlb d'15'
     clrf ANSELB, BANKED ; configurando el puerto B como digital
     clrf ANSELC, BANKED ; configurando el puerto C como digital
     clrf ANSELD, BANKED ; configurando el puerto D como digital
+    clrf ANSELA, BANKED	; configura como digital
     clrf TRISD, A ; configurando el puerto D como salida
     setf TRISB, A ; configurando el puerto B como entrada
     setf TRISC, A ; configurando el puerto C como entrada
+    clrf TRISA, A ; activa el puerto A como salida
+    #define RS LATA, 1, A
+    #define E LATA, 2, A
+    #define RW LATA, 3, A
+    #define dataLCD LATD, A
     
 start 
-    ; configuracion de interrupciones
+    ; configuracion de interrupciones ------------------------------------------
     bcf INTCON, 7, A ; activa prioridades
     movlw b'11101000' ; configuracion de INTCON
     movwf INTCON, A
@@ -36,9 +42,85 @@ start
     movlw b'00000001' ; activacion del IOC en el pin RC0
     movwf IOCC, A
     
+    ; Se define el valor inicial del registro para retardo para LCD ------------
+    movlw .247
+    movwf 0x32, A
+    
+    ; retardo inicial para que la LCD se inicialice ----------------------------
+    call ret40
+    movlw .247
+    movwf 0x32, A
+    
+    
+    ; Empieza a escribir en la LCD ---------------------------------------------
+    bcf RS			; se pone en 0 el RS porque aun no se necesita
+    ; Modo de funcionamiento de 2 lineas 
+    movlw b'00111000'
+    call enviaDatos
+    ; Encender el display, el cursor y el parpadeo 
+    movlw b'00001111'
+    call enviaDatos
+    ; Configurar el incremento del cursor hacia la derecha
+    movlw b'00010100'
+    call enviaDatos
+    ; Limpiar el display y enviar al home (posicion 0)
+    bcf RS
+    bcf RW
+    bsf E
+    movlw b'00000001'
+    movwf dataLCD
+    nop
+    bcf E
+    call ret2ms
+    nop
+    
+    ; Moverse a la posición 5 de la segunda línea (0x45) -----------------------
+    movlw b'11000101'	; se carga el 0x45 en binario
+			; el bit 7 se pone en 1 por sintaxis de Set DDRAM address
+    call enviaDatos
+    bsf RS			; ya se pone en 1 el RS para escribir los c y 4
+    ; Escribir la letra 'c' ----------------------------------------------------
+    movlw 'c'
+    call enviaDatos
+    ; Escribir el numero '4' ---------------------------------------------------
+    movlw '4'
+    call enviaDatos
+    
+    
 loop goto loop ; loop infinito para esperar a que se presione RB4 o RC0
     
     
+    ; Subrutina para protocolo de envio de datos al LCD ------------------------
+enviaDatos 
+    bcf RW
+    bsf E
+    movwf dataLCD
+    nop
+    bcf E
+    call ret40
+    movlw .247
+    movwf 0x32, A
+    
+    ; Retardos para LCD --------------------------------------------------------
+ret40 incf 0x32, F, A
+    btfss STATUS, 1
+    goto ret40
+    return
+    
+ret1ms movlw .8
+    movwf 0x33, A
+incRut incf 0x33, F, A
+    btfss STATUS, 2
+    goto incRut
+    return
+    
+ret2ms call ret1ms
+    call ret1ms
+    return
+    
+    
+    
+    ; Rutinas de interrupciones ------------------------------------------------
     org 0x100
 tmr0int 
     bcf LATD, 0, A ; apaga el LED en el pin RD0
