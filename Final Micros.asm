ff2313fc672a87c806082c15055f125cc15dc641
Marco Ortiz <m-ortiz99@hotmail.com>
2020-05-23 20:42:20 -0500

    Display flechas y corazones, y pantallas Win/Lose

--- Final Micros.asm
+++ Final Micros.asm
@@ -81,15 +81,13 @@ start
     call enviaDatos
     movlw b'00010100' ; Configurar el incremento del cursor hacia la derecha
     call enviaDatos
+reiniciaJuego   ; Aqui viene cuando se acaba el juego o se presiona la interrupcion de Abortar
     ; Limpiar el display y enviar al home (posicion 0)
-    bcf RS
-    bcf RW
-    bsf E
-    movlw b'00000001'
-    movwf dataLCD
+    call limpiaDisplay
     nop
-    bcf E
-    call ret2ms
+
+    ; Creación de custom characters para LCD -------------------------------------------------------------------------------
+    call crearCustoms
     nop
     
     ; Aqui empieza el menu (cartel) de Bienvenida --------------------------------------------------------------------------
@@ -160,14 +158,7 @@ checkBotonWelcome ; cambio de pantalla al presionar el boton RE0
     ; si se presiona, cambia de pantalla
 
     ; Limpiar el display y enviar al home (posicion 0)
-    bcf RS
-    bcf RW
-    bsf E
-    movlw b'00000001'
-    movwf dataLCD
-    nop
-    bcf E
-    call ret2ms
+    call limpiaDisplay
     nop
     ; Moverse a la posicion 1 de la primera linea (0x01) 
     movlw b'10000001' ; se carga el 0x01 en binario, el b7 es 1 por sintaxis de Set DDRAM address
@@ -250,9 +241,45 @@ checkBotonMode                  ; cambia de pantalla el modo que se haya selecci
         goto viewScore          ; si se presiono, va al marcador
     goto checkBotonMode         ; si no se presiono, vuelve a checar el otro boton
 
+
+
+    ; Pantalla de Play Game ------------------------------------------------------------------------------------------------
+
 playGame                        ; se queda esperando el potenciometro y seleccion del numero (no implementado)    
     movlw d'6'                  ; se carga la cantidad de vidas
     movwf vidas  
+    ; Limpiar el display y enviar al home (posicion 0)
+    call limpiaDisplay
+    nop
+
+    ; FALTA desplegar el numero a elegir en la primera linea
+
+    ; Despliegue de vidas en la segunda linea
+    ; Moverse a la posicion 5 de la segunda linea (0x45)
+    bcf RS 
+    movlw b'11000101' ; se carga el 0x45 en binario, el b7 es 1 por sintaxis de Set DDRAM address
+    call enviaDatos ; se envian los datos
+    bsf RS			; ya se pone en 1 el RS para escribir el mensaje
+    ; Se escribe el custom caracter del corazon y se forma "<3 = "
+    movlw 0x04      ; parte izquierda del corazon
+    call enviaDatos
+    movlw 0x03      ; parte derecha del corazon
+    call enviaDatos
+    movlw ' ' 
+    call enviaDatos
+    movlw '='
+    call enviaDatos
+    movlw ' ' 
+    call enviaDatos 
+
+    ; Se muestra el numero de vidas 
+    movf vidas, W, A    ; se cargan las vidas al WREG (en decimal)
+    addlw d'48'         ; le sumamos un 48 decimal (30 hex) que corresponde al '0' en ASCII
+    call enviaDatos     ; se despliegan las vidas
+
+
+    ; Pantalla de seleccionar numero ---------------------------------------------------------------------------------------
+
 selecNum
     movlw d'150'                ;[TODO]Supongamos que el usuarios escoge el num 150 con el potenciometro
                                 ;momentareamente usaremos numeros hardcodeados para simular la seleccion de los numeros
@@ -272,45 +299,244 @@ selecNum
         goto arrowDown
     goto arrowUp                ;[TODO] goto o call Â¿?
 
+
+
 ;Seccion de codigo que indica si se gano o perdio----------------------------------------------------------------------------------------
 
-gameWon                          ; [TODO] Implementar lÃ³gica para prender el led de ganador y para regresar al menu principal
+gameWon                         ; [TODO] Implementar lÃ³gica para prender el led de ganador y para regresar al menu principal
     incf numWins, 1, A
+    bsf ledWin                  ; se prende el LED de Win
 
-    goto gameWon
+    ; Guardar el numero de victorias en la EEPROM
+
+    ; Limpiar el display y enviar al home (posicion 0)
+    call limpiaDisplay
+    nop
+    bcf RS
+    ; Moverse a la posicion 4 de la primera linea (0x04) 
+    movlw b'10000100' ; se carga el 0x04 en binario, el b7 es 1 por sintaxis de Set DDRAM address
+    call enviaDatos ; se envian los datos
+    bsf RS			; ya se pone en 1 el RS para escribir el mensaje
+    ; Se empieza a escribir el mensaje letra por letra 
+    movlw 'Y'
+    call enviaDatos
+    movlw 'O'
+    call enviaDatos
+    movlw 'U'
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw 'W'
+    call enviaDatos
+    movlw 'I'
+    call enviaDatos
+    movlw 'N'
+    call enviaDatos
+    movlw '!'
+    call enviaDatos
+    bcf RS ; vuelve a poner el RS en 0 para mover la posicion del cursor
+    ; Moverse a la posicion 3 de la segunda linea (0x43)
+    movlw b'11000011' ; se carga el 0x40 en binario, el b7 es 1 por sintaxis de Set DDRAM address
+    call enviaDatos ; se envian los datos
+    bsf RS ; se pone el RS en 1 para escribir los nuevos valores
+    ; Se escribe el mensaje letra por letra
+    movlw 'A'
+    call enviaDatos
+    movlw 'G'
+    call enviaDatos
+    movlw 'A'
+    call enviaDatos
+    movlw 'I'
+    call enviaDatos
+    movlw 'N'
+    call enviaDatos
+    movlw '?'
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw 'R'
+    call enviaDatos
+    movlw 'E'
+    call enviaDatos
+    movlw '0'
+    call enviaDatos
+
+checkNewGame
+    btfss botonA            ; Checa si se presiono el botonA (RE0)
+        goto checkNewGame   ; Si no se ha presionado, vuelve a checar 
+    bcf ledWin              ; Si se presiona, apaga el led y reinicia el juego
+    goto reiniciaJuego      ; Reinicia el juego
+
+
 
 gameOver                         ;[TODO] Implementar lÃ³gica para prender el led de perdedor y para regresar al menu principal
     incf numDefeats, 1, A
+    bsf ledLose
+
+    ; Guardar el numero de defeats en la EEPROM
+
+    ; Limpiar el display y enviar al home (posicion 0)
+    call limpiaDisplay
+    nop
+    bcf RS
+    ; Moverse a la posicion 3 de la primera linea (0x03) 
+    movlw b'10000011' ; se carga el 0x04 en binario, el b7 es 1 por sintaxis de Set DDRAM address
+    call enviaDatos ; se envian los datos
+    bsf RS			; ya se pone en 1 el RS para escribir el mensaje
+    ; Se empieza a escribir el mensaje letra por letra 
+    movlw 'G'
+    call enviaDatos
+    movlw 'A'
+    call enviaDatos
+    movlw 'M'
+    call enviaDatos
+    movlw 'E'
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw 'O'
+    call enviaDatos
+    movlw 'V'
+    call enviaDatos
+    movlw 'E'
+    call enviaDatos
+    movlw 'R'
+    call enviaDatos
+    movlw '!'
+    call enviaDatos
+    bcf RS ; vuelve a poner el RS en 0 para mover la posicion del cursor
+    ; Moverse a la posicion 3 de la segunda linea (0x43)
+    movlw b'11000011' ; se carga el 0x40 en binario, el b7 es 1 por sintaxis de Set DDRAM address
+    call enviaDatos ; se envian los datos
+    bsf RS ; se pone el RS en 1 para escribir los nuevos valores
+    ; Se escribe el mensaje letra por letra
+    movlw 'R'
+    call enviaDatos
+    movlw 'E'
+    call enviaDatos
+    movlw 'T'
+    call enviaDatos
+    movlw 'R'
+    call enviaDatos
+    movlw 'Y'
+    call enviaDatos
+    movlw '?'
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw 'R'
+    call enviaDatos
+    movlw 'E'
+    call enviaDatos
+    movlw '0'
+    call enviaDatos
+
+checkRetry
+    btfss botonA            ; Checa si se presiono el botonA (RE0)
+        goto checkRetry     ; Si no se ha presionado, vuelve a checar 
+    bcf ledLose             ; Si se presiona, apaga el led y reinicia el juego
+    goto reiniciaJuego      ; Reinicia el juego
 
 
-    goto gameOver
 
-;Seccion de codigo para pinta los custom characters----------------------------------------------------------------------------------------
-arrowUp                          ;[TODO] Implementar lógica para pintar el custom que indicará al jugador escojer un numero mayor
+    ; Despliegue de flechas hacia arriba -----------------------------------------------------------------------------------
+arrowUp
+    ; Moverse a la posicion 3 de la primera linea (0x03) para desplegar flecha hacia arriba
+    bcf RS
+    movlw b'10000011' ; se carga el 0x03 en binario, el b7 es 1 por sintaxis de Set DDRAM address
+    call enviaDatos ; se envian los datos
+    bsf RS			; ya se pone en 1 el RS para escribir el mensaje
+    ; Se escriben 4 flechas hacia arriba en la primera linea de la LCD
+    movlw 0x00
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw 0x00
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw 0x00
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw 0x00
+    call enviaDatos
+
+    ; Moverse a la posicion 10 de la segunda linea (0x50) para desplegar nuevo numero de vidas
+    bcf RS
+    movlw b'11010000' ; se carga el 0x50 en binario, el b7 es 1 por sintaxis de Set DDRAM address
+    call enviaDatos ; se envian los datos
+    bsf RS			; ya se pone en 1 el RS para escribir el mensaje
+    ; Se escribe la nueva cantidad de vidas
+    movf vidas, W, A    ; se cargan las vidas al WREG (en decimal)
+    addlw d'48'         ; le sumamos un 48 decimal (30 hex) que corresponde al '0' en ASCII
+    call enviaDatos     ; se despliegan las vidas
+
+checkArrowUp ; cambio de pantalla al presionar el boton RE0
+    btfss botonA ; checa si se presiono el boton para pasar de pantalla
+        goto checkArrowUp ; si no se presiono, se queda esperando
+    goto selecNum         ; si se presiona, cambia de pantalla y se regresa a seleccionar un numero
 
 
 
+    ; Despliegue de las flechas hacia abajo --------------------------------------------------------------------------------
 
-    goto selecNum                ;Se regresa para seleccionar el num
+arrowDown
+; Moverse a la posicion 3 de la primera linea (0x03) para desplegar flecha hacia abajo
+    bcf RS
+    movlw b'10000011' ; se carga el 0x03 en binario, el b7 es 1 por sintaxis de Set DDRAM address
+    call enviaDatos ; se envian los datos
+    bsf RS			; ya se pone en 1 el RS para escribir el mensaje
+    ; Se escriben 4 flechas hacia abajo en la primera linea de la LCD
+    movlw 0x01
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw 0x01
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw 0x01
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw ' '
+    call enviaDatos
+    movlw 0x01
+    call enviaDatos
 
-arrowDown                          ;[TODO] Implementar lógica para pintar el custom que indicará al jugador escojer un numero menor
+    ; Moverse a la posicion 10 de la segunda linea (0x50) para desplegar nuevo numero de vidas
+    bcf RS
+    movlw b'11010000' ; se carga el 0x50 en binario, el b7 es 1 por sintaxis de Set DDRAM address
+    call enviaDatos ; se envian los datos
+    bsf RS			; ya se pone en 1 el RS para escribir el mensaje
+    ; Se escribe la nueva cantidad de vidas
+    movf vidas, W, A    ; se cargan las vidas al WREG (en decimal)
+    addlw d'48'         ; le sumamos un 48 decimal (30 hex) que corresponde al '0' en ASCII
+    call enviaDatos     ; se despliegan las vidas
+
+checkArrowDown ; cambio de pantalla al presionar el boton RE0
+    btfss botonA ; checa si se presiono el boton para pasar de pantalla
+        goto checkArrowDown ; si no se presiono, se queda esperando
+    goto selecNum           ; si se presiona, cambia de pantalla y se regresa a seleccionar un numero
 
 
 
-
-    goto selecNum                ;Se regresa para seleccionar el num
-
+    ; Pantalla para ver el Marcador ----------------------------------------------------------------------------------------
 
 viewScore ; se muestra el marcador en la pantalla del LCD (Falta lo de la memoria de la EEPROM)
     ; Limpiar el display y enviar al home (posicion 0)
-    bcf RS
-    bcf RW
-    bsf E
-    movlw b'00000001'
-    movwf dataLCD
-    nop
-    bcf E
-    call ret2ms
+    call limpiaDisplay
     nop
     ; Moverse a la posicion 3 de la primera linea (0x03) 
     movlw b'10000011' ; se carga el 0x03 en binario, el b7 es 1 por sintaxis de Set DDRAM address
@@ -359,7 +585,101 @@ loop        ; loop infinito para pausar el programa mientras debuggeo
     goto loop 
 
     
+    ; Creación de custom characters para LCD -------------------------------------------------------------------------------
+crearCustoms
+    bcf RS
+    movlw b'01000000'       ; va a la direccion 0x00 de la CGRAM 
+    call enviaDatos
+    ; Creación de la Flecha hacia arriba (Caracter 0x00)
+    bsf RS
+    movlw b'00000000'
+    call enviaDatos
+    movlw b'00000100'
+    call enviaDatos
+    movlw b'00001110'
+    call enviaDatos
+    movlw b'00011111'
+    call enviaDatos
+    movlw b'00001110'
+    call enviaDatos
+    movlw b'00001110'
+    call enviaDatos
+    movlw b'00001110'
+    call enviaDatos
+    movlw b'00000000'
+    call enviaDatos
+    ; Creación de la Flecha hacia abajo (Caracter 0x01)
+    movlw b'00000000'
+    call enviaDatos
+    movlw b'00001110'
+    call enviaDatos
+    movlw b'00001110'
+    call enviaDatos
+    movlw b'00001110'
+    call enviaDatos
+    movlw b'00011111'
+    call enviaDatos
+    movlw b'00001110'
+    call enviaDatos
+    movlw b'00000100'
+    call enviaDatos
+    movlw b'00000000'
+    call enviaDatos
+    ; Creación de la parte izquierda del corazon (Caracter 0x02)
+    movlw b'00000110'
+    call enviaDatos
+    movlw b'00001111'
+    call enviaDatos
+    movlw b'00011111'
+    call enviaDatos
+    movlw b'00011111'
+    call enviaDatos
+    movlw b'00001111'
+    call enviaDatos
+    movlw b'00000111'
+    call enviaDatos
+    movlw b'00000011'
+    call enviaDatos
+    movlw b'00000001'
+    call enviaDatos
+    ; Creación de la parte derecha del corazon (Caracter 0x03)
+    movlw b'00001100'
+    call enviaDatos
+    movlw b'00011110'
+    call enviaDatos
+    movlw b'00011111'
+    call enviaDatos
+    movlw b'00011111'
+    call enviaDatos
+    movlw b'00011110'
+    call enviaDatos
+    movlw b'00011100'
+    call enviaDatos
+    movlw b'00011000'
+    call enviaDatos
+    movlw b'00010000'
+    call enviaDatos
+
+
+    bcf RS ; regresar RS a 0 porque asi estaba antes del call
+    return ; regresa del call de la linea 95 y continua con la inicializacion del codigo
     
+
+
+    ; Subrutina para limpiar el display y enviar a la posicion 0 -----------------------------------------------------------
+limpiaDisplay
+    bcf RS
+    bcf RW
+    bsf E
+    movlw b'00000001'
+    movwf dataLCD
+    nop
+    bcf E
+    call ret2ms
+    nop
+    return
+
+
 
     ; Subrutina para protocolo de envio de datos al LCD --------------------------------------------------------------------
 enviaDatos 
